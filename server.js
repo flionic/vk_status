530671ea8222959a742bb925ffa38b3aeb432462
Bionic-Leha <bionic.mods@gmail.com>
2017-03-26 18:06:18 +0300

    Change server.js

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(8080, function(){
-    console.log('Server running on 8080...');
-});
+var http = require('http');
+var static = require('node-static');
+var file = new static.Server('.');
+
+http.createServer(function(req, res) {
+  file.serve(req, res);
+}).listen(8080);
+
+console.log('Server running on port 8080');
