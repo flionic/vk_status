4d337668cd54e7889ec799e332ae06fb13badf52
Bionic-Leha <bionic.mods@gmail.com>
2017-03-26 18:14:41 +0300

    Upd server for index.html

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  //response.send('Hello World!');
+  response.sendFile(path.join(__dirname + '/index.html'));
 });
 
 var port = process.env.PORT || 5000;
