5720b1dae2921ba24fe984aebc6bd076a8c9ca5a
Bionic-Leha <bionic.mods@gmail.com>
2017-03-26 18:22:00 +0300

    Upd node server (2)

--- server.js
+++ server.js
@@ -1,14 +1,10 @@
-var express = require("express");
+var express = require('express');
 var app = express();
 var path = require('path');
-app.use(express.logger());
 
-app.get('/', function(request, response) {
-  //response.send('Hello World!');
-  response.sendFile(path.join(__dirname + '/index.html'));
+// viewed at http://localhost:8080
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+app.listen(8080);
