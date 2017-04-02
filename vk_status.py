b40b330814d9866bf350cb1a55c2cc2c77091f38
Bionic-Leha <bionic.mods@gmail.com>
2017-04-02 21:16:13 +0300

    Test sql query

--- vk_status.py
+++ vk_status.py
@@ -20,7 +20,8 @@ cur.execute("SELECT id FROM users;")
 for row in cur:
     print(row)
 
-cur.execute("INSERT INTO users ('id') VALUES ('2281488')")
+cur.execute("INSERT INTO users VALUES ('1488228')")
+#cur.execute("INSERT INTO `users` (`id`) VALUES ('111222333')")
 #cur.execute("DELETE FROM users WHERE id='122734122'")
 
 cur.close()
