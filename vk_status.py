6d90f3e13dfbef5119100fead832493635fd85e0
Bionic-Leha <bionic.mods@gmail.com>
2017-04-03 06:37:20 +0300

    Added offset +3 hours [fix]

--- vk_status.py
+++ vk_status.py
@@ -215,7 +215,7 @@ def parseFeed(force=False, fid=''):
 	try:
 		rss = ET.fromstring(requests.get('https://freelance.ua/orders/rss').text.encode('utf-8'))
 		locale.setlocale(locale.LC_TIME, "en_US.UTF-8")
-		rssPubDate = datetime.strptime(rss[0][5].text, '%a, %d %b %Y %H:%M:%S %z').timestamp() + 10850
+		rssPubDate = datetime.strptime(rss[0][5].text, '%a, %d %b %Y %H:%M:%S %z').timestamp() + 10750
 		if (rssPubDate > rssUpdDate) or force:
 			print('Parsing freelance.ua...')
 			soup = BeautifulSoup(session.get('https://freelance.ua/').text, "lxml")
